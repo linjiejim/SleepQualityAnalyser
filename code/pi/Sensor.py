d7245b30c2f072e407a0a52b71812379053de31f
Jim Lin <linjiejim@live.com>
2019-10-22 14:12:30 +1100

    Create Sensor.py
    define Sensor class

---
+++ code/pi/Sensor.py
@@ -0,0 +1,35 @@
+
+'''
+Supported Sensors
+
+* Temperature and Humidity 
+* Brightness 
+* Noise
+* Motion
+* AirQuality
+'''
+
+# Import packages
+import 
+
+# Pins connection
+PIN_DHT11 = 
+
+class Sensor:
+	def __init__(self): 
+		pass
+
+	def read_temperature_humidity(self):
+		return temp, humi 
+
+	def read_brightness(self):
+		return brig 
+
+	def read_noise(self):
+		return nois 
+
+	def read_rip(motion):
+		return moti 
+
+	def read_air_quality(self):
+		return airq 
